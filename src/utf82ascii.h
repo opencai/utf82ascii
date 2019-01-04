9b22cbaa4a438907e6a9986448d19421b3cf2d0e
hxz <opencai@126.com>
2019-01-04 17:24:20 +0800

    first commit

---
+++ src/utf82ascii.h
@@ -0,0 +1,6 @@
+#ifndef __UTF82ASCII_H__
+#define __UTF82ASCII_H__
+
+char* utf82ascii(const char* utf8);
+
+#endif /*__UTF82ASCII_H__*/
---
+++ test/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+#include "utf82ascii.h"
+
+int main(int argc, char* argv[])
+{
+	string text = u8"©Ẹa12";
+
+	text = u8"Žluťoučký kůň";
+	text = u8"naïve!!!##$";
+	text = u8"tréma";
+	text = u8"ʿABBĀSĀBĀD|哈哈ok123";
+	text = u8"Höhle";
+	text = u8"tréma";
+	text = utf82ascii(text.c_str());
+
+	return 0;
+}
+
